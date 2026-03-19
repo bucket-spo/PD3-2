cb6d1f304fe8a8972e804fab44e23840e3ffac79
bucket-spo <bucket.spo@gmail.com>
2026-03-19 10:21:34 +0200

    Update 1_procesi.ps1

--- 1_procesi.ps1
+++ 1_procesi.ps1
@@ -1,2 +1,3 @@
-$Arhivs = "$env:USERPROFILE\Documents"
-Get-Process | where-object {$_.WorkingSet -gt 157286400 -and -notlike $_.ProcessName "msedge"}  | Export-csv "LielieProcesi.csv"
+
+$bumba = Get-Process | where-object {$_.WorkingSet -gt 157286400 -and $_.Name -ne "msedge"}
+$bumba | select-object Name, Id, workingSet | Export-csv "$env:USERPROFILE\Documents\LielieProcesi.csv" -NoTypeInformation -encoding UTF8
